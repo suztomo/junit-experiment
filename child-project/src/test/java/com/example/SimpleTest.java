8dcd3fa6783ec1c3f5806b17bbdaa2586e70b595
Mridula Peddada <mpeddada@google.com>
2022-01-28 13:57:25 -0500

    Category annotation and initialize TestTag at build time

--- child-project/src/test/java/com/example/SimpleTest.java
+++ child-project/src/test/java/com/example/SimpleTest.java
@@ -3,7 +3,12 @@ package com.example;
 import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
+@Category(MyTestCategory.class)
 public class SimpleTest {
 
   @Test
